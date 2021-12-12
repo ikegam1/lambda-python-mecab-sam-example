e3de7e03b334886f11560b16b6d51434fd391555
ikegam1 <takesiba.masrao@gmail.com>
2021-12-12 22:24:29 +0900

    querystringに対応

--- hello_world/app.py
+++ hello_world/app.py
@@ -10,21 +10,21 @@ libmecab = ctypes.cdll.LoadLibrary(os.path.join(libdir, 'libmecab.so'))
 import MeCab
 
 # IPA辞書を利用
-ipadic_tagger = MeCab.Tagger('-O wakati -r /dev/null -d /var/task/lib/mecab/dic/ipadic')
+ipadic_tagger = MeCab.Tagger('-r /dev/null -d /var/task/lib/mecab/dic/ipadic')
 # NEologdを利用
-neologd_tagger = MeCab.Tagger('-O wakati -r /dev/null -d /var/task/lib/mecab/dic/mecab-ipadic-neologd')
+neologd_tagger = MeCab.Tagger('-r /dev/null -d /var/task/lib/mecab/dic/mecab-ipadic-neologd')
 
 
 def lambda_handler(event, context):
     
+    keyword = event['queryStringParameters']['keyword']
 
-    text = '中居正広の金曜日のスマイルたちへ'
-    print (ipadic_tagger.parse(text))
-    print (neologd_tagger.parse(text))
+    print (ipadic_tagger.parse(keyword))
+    print (neologd_tagger.parse(keyword))
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "ipadic": ipadic_tagger.parse('中居正広の金曜日のスマイルたちへ'),
-            "neologd": neologd_tagger.parse('中居正広の金曜日のスマイルたちへ')
+            "ipadic": ipadic_tagger.parse(keyword),
+            "neologd": neologd_tagger.parse(keyword)
         }),
     }
